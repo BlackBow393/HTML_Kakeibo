a83b2bb2a952571a79c058831b7f126969cfe06e
BlackBow393 <t9374bow86@outlook.com>
2025-08-10 11:40:17 +0900

    config.pyを修正

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # config.py
-import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_FILE = os.path.join(BASE_DIR, "kakeibo.db")
+#import os
+from pathlib import Path
+#BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = Path(r"C:\Users\t9374\OneDrive\デスクトップ\家計簿\プログラム家計簿")
+DB_FILE = BASE_DIR / "kakeibo.db"
